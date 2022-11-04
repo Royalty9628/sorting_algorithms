da2da9636ee3612c5da6041847f8a660c7080657
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-11-04 11:50:54 -0700

    Selection Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,52 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
-/**
-* insertion_sort_list - sorts a doubly linked list of integers in
-* ascending order using the Insertion sort algorithm
-* @list: List to order
+* insertion_sort_list - sorts a doubly linked list of integers
+* using the insertion sort algorithm
+* @list: A pointer to the head of a doubly-linked list of integers
 * Return : Void
 */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *temp;
+	listint_t *aux2 = NULL, *aux_prev = NULL, *aux_next = NULL, *tmp = NULL;
+	int wall = 0;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list)
 		return;
-	for (iter = (*list)->next; iter != NULL; iter = temp)
+
+	tmp = *list;
+
+	while (tmp)
 	{
-		temp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		if (tmp->prev != NULL)
 		{
-			swap_node(list, &insert, iter);
-			print_list((const listint_t *)*list);
+			aux2 = tmp;
+			wall = 0;
+			while (aux2 && aux2->prev->n > aux2->n)
+			{
+				aux_prev = aux2->prev;
+				aux_next = aux2->next;
+
+				if (aux_prev->prev)
+					aux_prev->prev->next = aux2;
+				else
+				{
+					*list = aux2;
+					wall = 1;
+				}
+				if (aux_next)
+					aux_next->prev = aux_prev;
+
+				aux2->prev = aux_prev->prev;
+				aux2->next = aux_prev;
+				aux_prev->prev = aux2;
+				aux_prev->next = aux_next;
+				print_list(*list);
+				if (wall)
+					break;
+			}
 		}
+		tmp = tmp->next;
 	}
 }
