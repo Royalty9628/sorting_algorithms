9b5892448d4386a91abcccef59b3765d709235d2
Royalty9628 <ejaromedoghene.eruoma@gmail.com>
2022-11-05 07:19:46 -0700

    Sort Ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	int swap, swapped;
+	size_t i;
 
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
 
-	for (i = 0; i < size - 1; i++)
+	while (swapped)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array [j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swapped = 1;
+				swap = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = swap;
 				print_array(array, size);
 			}
 		}
